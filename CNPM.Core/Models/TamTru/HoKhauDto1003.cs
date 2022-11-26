3b0bb888e412816661870436641cd4f8642c68a0
Ngo Duc Thang <ngoducthang06072001@gmail.com>
2022-11-26 11:05:22 +0700

    add model tam tru

---
+++ CNPM.Core/Models/TamTru/HoKhauDto1003.cs
@@ -0,0 +1,30 @@
+﻿using CNPM.Core.Utils;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CNPM.Core.Models.HoKhau
+{
+    public class HoKhauDto1003 // body get list
+    {
+        [JsonProperty("maHoKhau")]
+        public string MaHoKhau { get; set; }
+
+        [JsonProperty("soThanhVien")]
+        public int SoThanhVien { get; set; }
+
+        [JsonProperty("diaChiThuongTru")]
+        public string DiaChiThuongTru { get; set; }
+
+        [JsonProperty("noiCap")]
+        public string NoiCap { get; set; }
+
+        [JsonProperty("ngayCap")]
+        public DateTime NgayCap { get; set; }
+    }
+}
+
